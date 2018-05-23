aa28279552135954875a60e6a5d3506828bdc1cc
SEUXXD <894873161@qq.com>
2018-05-23 11:20:55 +0800

    final version

--- app/src/main/java/Constant/URLConstant.java
+++ app/src/main/java/Constant/URLConstant.java
@@ -6,8 +6,8 @@ package Constant;
 
 public class URLConstant {
 //    public static final String BASE_URL = "http://211.149.209.11:53825/db/";
-//    public static final String BASE_URL = "http://192.168.0.102:8080/db/";
-    public static final String BASE_URL = "http://gy171217.vicp.io:11587/db/";
+    public static final String BASE_URL = "http://192.168.0.102:8080/db/";
+//    public static final String BASE_URL = "http://gy171217.vicp.io:11587/db/";
     public static final String VIDEO_URL = "http://192.168.0.102:8080/text/";
 //    public static final String LOGIN_URL = "http://122.226.45.78:7080/unit/";
     public static final String TEST_PDF_URL = "http://101.132.154.189/test/upload/";
